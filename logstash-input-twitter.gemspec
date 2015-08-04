e32ec2ed62e4ea5c94135a5136dc543e59b8b1bd
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2015-08-04 17:04:42 -0300

    starting tests

--- logstash-input-twitter.gemspec
+++ logstash-input-twitter.gemspec
@@ -8,7 +8,7 @@ Gem::Specification.new do |s|
   s.authors         = ["Elastic"]
   s.email           = 'info@elastic.co'
   s.homepage        = "http://www.elastic.co/guide/en/logstash/current/index.html"
-  s.require_paths = ["lib"]
+  s.require_paths   = ["lib"]
 
   # Files
   s.files = `git ls-files`.split($\)+::Dir.glob('vendor/*')
@@ -23,5 +23,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency "logstash-core", '>= 1.4.0', '< 2.0.0'
   s.add_runtime_dependency 'twitter', ['5.12.0']
 
+  s.add_development_dependency 'logstash-codec-plain'
   s.add_development_dependency 'logstash-devutils'
+  s.add_development_dependency 'rspec-mocks'
 end
--- spec/inputs/twitter_spec.rb
+++ spec/inputs/twitter_spec.rb
@@ -1,5 +1,46 @@
 require "logstash/devutils/rspec/spec_helper"
-require 'logstash/inputs/twitter'
+require "logstash/codecs/plain"
+require 'lib/logstash/inputs/twitter'
+require "twitter"
 
 describe LogStash::Inputs::Twitter do
+   let(:queue) { Queue.new }
+   let(:klass) { LogStash::Inputs::Twitter }
+   let(:twitter_instance) { object_spy(Twitter::Streaming::Client.new) }
+   let(:tweet_mock) { Twitter::Tweet.new :id => 1 }
+   let(:keywords) { ["keywords", "keywords2"] }
+   let(:follows) { ["follows", "follows2"] }
+   let(:locations) { ["locations", "locations2"] }
+   let(:default_opts) {
+     {
+       "consumer_key" => "consumer_key",
+       "consumer_secret" => "consumer_secret",
+       "oauth_token" => "oauth_token",
+       "oauth_token_secret" => "oauth_token_secret",
+       "keywords" => :keywords,
+       "follows" => :follows,
+       "locations" => :locations
+     }
+   }
+   subject(:twitter_plugin) { klass.new(default_opts) }
+
+   context "#register" do
+     it "raise no exception" do
+       plugin = klass.new(default_opts)
+       expect { plugin.register }.not_to raise_error
+     end
+   end
+
+   before do
+     twitter_plugin.register
+   end
+
+   describe "#run" do
+     it "should run only with track, follow and location and return tweet" do
+       twitter_instance.stub(:filter).and_return(false) #improve
+       twitter_plugin.run(:queue)
+       expect(twitter_instance).to receive(:filter)
+       #expect(twitter_instance).to have_received(:filter).with(:keywords, :follows, :locations)
+     end
+   end
 end
